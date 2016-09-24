f887a0c7091ccbaaf42a8df860c59e854932f4f0
Cole Smith <css459@nyu.edu>
2016-09-24 18:16:02 -0400

    Initial Commit

---
+++ Hummingbird/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hummingbird
+//
+//  Created by Cole Smith on 9/24/16.
+//  Copyright © 2016 Ahmad and Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
