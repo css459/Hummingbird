a76d6f917acc386288237bedb045fc3486735b65
Cole Smith <css459@nyu.edu>
2016-09-25 13:59:16 -0400

    kickass login animations

--- Hummingbird/LoginViewController.swift
+++ Hummingbird/LoginViewController.swift
@@ -13,13 +13,40 @@ import Firebase
 
 class LoginViewController: UIViewController, FBSDKLoginButtonDelegate {
     
+    @IBOutlet weak var welcomeText: UILabel!
+    @IBOutlet weak var logoImage: UIImageView!
     @IBOutlet weak var facebookLogin: FBSDKLoginButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.facebookLogin.delegate = self
         self.facebookLogin.readPermissions = ["public_profile", "email", "user_friends"]
-
+        
+        // Pretty animations
+        let animationDuration = 2.0
+        let delayBetween: UInt32 = 1
+        welcomeText.alpha = 0.0
+        logoImage.alpha = 0.00
+        facebookLogin.alpha = 0.0
+        facebookLogin.isEnabled = false
+        UIView.animate(withDuration: animationDuration, animations: {
+            // Logo
+            self.logoImage.alpha = 1.0
+            }, completion: { _ in
+                // Text
+                sleep(delayBetween)
+                UIView.animate(withDuration: animationDuration, animations: {
+                    self.welcomeText.alpha = 1.0
+                    }, completion: { _ in
+                        // Button
+                        sleep(delayBetween)
+                        UIView.animate(withDuration: animationDuration, animations: {
+                            self.facebookLogin.alpha = 1.0
+                            }, completion: { _ in
+                                self.facebookLogin.isEnabled = true
+                        })
+                })
+        })
     }
     
     func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
